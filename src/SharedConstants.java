9edba4647fe6591205a88b55a4eff765c0ac7a78
pockocmoc <pockocmoc@inbox.ru>
2022-09-12 08:13:21 +0300

    Initial commit

---
+++ src/SharedConstants.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+
+interface SharedConstants {
+    int NO = 0;
+    int YES = 1;
+    int MAYBE = 2;
+    int LATER = 3;
+    int SOON = 4;
+    int NEVER = 5;
+}
+
+class Question implements SharedConstants {
+    Random rand = new Random();
+
+    int ask() {
+        int prob = (int) (100 * rand.nextDouble());
+        if (prob < 30)
+            return NO;   // 30%
+        else if (prob < 60)
+            return YES;  // 30%
+        else if (prob < 75)
+            return LATER;  // 15%
+        else if (prob < 98)
+                return SOON;  // 13%
+        else
+                return NEVER; // 2%
+
+            }
+        }
+
